15595c327f86f709f2a759286f10d92b33b2858b
cindybarbosa <cindy.barbosa@sptech.school>
2024-11-08 19:17:49 -0300

    Integração Slack com o Java

---
+++ src/main/java/MainSlack.java
@@ -0,0 +1,6 @@
+public class MainSlack {
+    public static void main(String[] args) {
+        // Enviando uma notificação para o Slack
+        SlackNotifier.sendNotification("TEste");
+    }
+}
---
+++ src/main/java/SlackNotifier.java
@@ -0,0 +1,35 @@
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SlackNotifier {
+    public static void sendNotification(String message) {
+        try {
+            String webhookUrl = "https://hooks.slack.com/services/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";
+            URL url = new URL(webhookUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            // Montando o payload com a formatação correta
+            String payload = "{\"text\":\"" + message + "\"}";
+
+            // Enviando o payload
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = payload.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // Verificando a resposta
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("Mensagem enviada com sucesso.");
+            } else {
+                System.out.println("Erro ao enviar a mensagem. Código de resposta: " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
